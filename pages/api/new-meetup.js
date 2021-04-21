2ad8016b9e1d27e8b34561dc69eb6f6e805adf4b
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-21 18:16:16 +0700

    add mongodb driver

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,27 @@
 //  /api/new-meetup
 
-function handler(req,res) {
-if(req.method === 'POST') {
-    const data = req.body;
-    
-    const {title, image, address, desciption} = data;
+import { MongoClient } from 'mongodb';
 
-    
-}
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://Nam-Nguyen-1990:vrmRmr4M3uljj9ij@cluster0.wnjfl.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
+  }
 }
 
 export default handler;
